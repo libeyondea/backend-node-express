a1a23cde2c015f603b12cf376937fe2f7f680983
de4th-zone <57558120+de4th-zone@users.noreply.github.com>
2021-09-11 22:49:04 +0700

    Update index.js

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,11 +2,9 @@ import { Router } from 'express';
 import authRoute from './authRoute';
 import userRoute from './userRoute';
 import roleRoute from './roleRoute';
-import setupRoute from './setupRoute';
 
 const router = Router();
 
-router.use('/setup', setupRoute);
 router.use('/auth', authRoute);
 router.use('/users', userRoute);
 router.use('/roles', roleRoute);
