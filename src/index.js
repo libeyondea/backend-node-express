100c7f4f42bc709e08b8ac92e8c0ce7b9d8fdfe4
de4th-zone <57558120+de4th-zone@users.noreply.github.com>
2021-09-11 22:48:53 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import mongoose from 'mongoose';
 import { DATABASE_URI, HOST, PORT } from '~/config/env';
 import app from '~/config/express';
 import logger from './config/logger';
+import Permission from './models/permission';
+import Role from './models/role';
+import User from './models/user';
 
 let server;
+
+mongoose.Promise = global.Promise;
 
 const db = mongoose.connection;
 
@@ -28,12 +33,122 @@ db.on('reconnected', () => {
 	logger.info('🚀 MongoDB reconnected!');
 });
 
+async function initial() {
+	try {
+		const countPermissions = await Permission.estimatedDocumentCount();
+		if (countPermissions === 0) {
+			await Permission.create(
+				{
+					controller: 'user',
+					action: 'create'
+				},
+				{
+					controller: 'user',
+					action: 'read'
+				},
+				{
+					controller: 'user',
+					action: 'update'
+				},
+				{
+					controller: 'user',
+					action: 'delete'
+				},
+				{
+					controller: 'role',
+					action: 'create'
+				},
+				{
+					controller: 'role',
+					action: 'read'
+				},
+				{
+					controller: 'role',
+					action: 'update'
+				},
+				{
+					controller: 'role',
+					action: 'delete'
+				}
+			);
+		}
+		const countRoles = await Role.estimatedDocumentCount();
+		if (countRoles === 0) {
+			const permissionsSuperAdministrator = await Permission.find();
+			const permissionsAdministrator = await Permission.find({ controller: 'user' });
+			const permissionsModerator = await Permission.find({ controller: 'user', action: { $ne: 'delete' } });
+			await Role.create(
+				{
+					name: 'Super Administrator',
+					permissions: permissionsSuperAdministrator
+				},
+				{
+					name: 'Administrator',
+					permissions: permissionsAdministrator
+				},
+				{
+					name: 'Moderator',
+					permissions: permissionsModerator
+				},
+				{
+					name: 'User',
+					permissions: []
+				}
+			);
+		}
+		const countUsers = await User.estimatedDocumentCount();
+		if (countUsers === 0) {
+			const roleSuperAdministrator = await Role.findOne({ name: 'Super Administrator' });
+			const roleAdministrator = await Role.findOne({ name: 'Administrator' });
+			const roleModerator = await Role.findOne({ name: 'Moderator' });
+			const roleUser = await Role.findOne({ name: 'User' });
+			await User.create(
+				{
+					firstName: 'Thuc',
+					lastName: 'Nguyen',
+					userName: 'superadmin',
+					email: 'superadmin@example.com',
+					password: 'superadmin',
+					roles: [roleSuperAdministrator, roleAdministrator, roleModerator, roleUser]
+				},
+				{
+					firstName: 'Vy',
+					lastName: 'Nguyen',
+					userName: 'admin',
+					email: 'admin@example.com',
+					password: 'admin',
+					roles: [roleAdministrator]
+				},
+				{
+					firstName: 'Thuyen',
+					lastName: 'Nguyen',
+					userName: 'moderator',
+					email: 'moderator@example.com',
+					password: 'moderator',
+					roles: [roleModerator]
+				},
+				{
+					firstName: 'Uyen',
+					lastName: 'Nguyen',
+					userName: 'user',
+					email: 'user@example.com',
+					password: 'user',
+					roles: [roleUser]
+				}
+			);
+		}
+	} catch (err) {
+		logger.error(err);
+	}
+}
+
 mongoose
 	.connect(DATABASE_URI, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true
 	})
 	.then(() => {
+		initial();
 		logger.info('🚀 Connected to MongoDB end!');
 		server = app.listen(PORT, HOST, () => {
 			logger.info(`🚀 Host: http://${HOST}:${PORT}`);
