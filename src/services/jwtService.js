574cfa85e595b3e6187a8f3045e2bd9e292a38a5
de4th-zone <57558120+de4th-zone@users.noreply.github.com>
2021-09-11 22:49:11 +0700

    Create jwtService.js

---
+++ src/services/jwtService.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+import moment from 'moment';
+import APIError from '~/utils/apiError';
+
+export const sign = (userId, expires, secret) => {
+	try {
+		const payload = {
+			sub: userId,
+			iat: moment().unix(),
+			exp: expires.unix()
+		};
+		return jwt.sign(payload, secret);
+	} catch (err) {
+		throw new APIError(err.message, 401, true);
+	}
+};
+
+export const verify = async (token, secret) => {
+	try {
+		return jwt.verify(token, secret);
+	} catch (err) {
+		throw new APIError(err.message, 401, true);
+	}
+};
