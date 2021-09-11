7dbf0bd6997704ef0ae1642e723bb85924963c29
de4th-zone <57558120+de4th-zone@users.noreply.github.com>
2021-09-11 22:49:07 +0700

    Delete setupRoute.js

--- src/routes/v1/setupRoute.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import catchAsync from '~/utils/catchAsync';
-import * as setupController from '~/controllers/setupController';
-
-const router = Router();
-
-router.post('/', catchAsync(setupController.setup));
-
-export default router;
