4e21c6a1c02f5d2451a0646f9f7bc5d977867803
de4th-zone <57558120+de4th-zone@users.noreply.github.com>
2021-09-12 19:24:22 +0700

    Update express.js

--- src/config/express.js
+++ src/config/express.js
@@ -6,14 +6,20 @@ import passport from './passport';
 import routes from '~/routes/v1';
 import * as error from '~/middlewares/error';
 import rateLimiter from '~/middlewares/rateLimiter';
+import { NODE_ENV } from './env';
+import morgan from './morgan';
 
 const app = express();
+
+if (NODE_ENV !== 'test') {
+	app.use(morgan);
+}
 
 app.use(helmet());
 
 app.use(express.json());
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(compression());
 
@@ -22,6 +28,8 @@ app.use(cors());
 app.use(rateLimiter);
 
 app.use(passport.initialize());
+
+app.use(express.static('public'));
 
 app.use('/api/v1', routes);
 
