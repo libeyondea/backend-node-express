0924b42ed4f256d328eb5c1c1db95e1db7781ac9
de4th-zone <57558120+de4th-zone@users.noreply.github.com>
2021-09-12 19:24:44 +0700

    Create imageRoute.js

---
+++ src/routes/v1/imageRoute.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import catchAsync from '~/utils/catchAsync';
+import * as imageController from '~/controllers/imageController';
+import uploadImage from '~/middlewares/uploadImage';
+import authenticate from '~/middlewares/authenticate';
+
+const router = Router();
+
+router.post('/upload', authenticate(), uploadImage, catchAsync(imageController.uploadImage));
+
+export default router;
