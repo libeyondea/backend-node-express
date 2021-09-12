992f5864e51b7a10c88cb7f4f44d57cfc45fd60b
de4th-zone <57558120+de4th-zone@users.noreply.github.com>
2021-09-12 19:24:24 +0700

    Update logger.js

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,10 +1,28 @@
 import winston from 'winston';
+import { NODE_ENV } from './env';
+
+const levels = {
+	error: 0,
+	warn: 1,
+	info: 2,
+	http: 3,
+	debug: 4
+};
+
+winston.addColors({
+	error: 'red',
+	warn: 'yellow',
+	info: 'green',
+	http: 'magenta',
+	debug: 'white'
+});
 
 const logger = winston.createLogger({
-	level: 'info',
+	level: NODE_ENV === 'development' ? 'debug' : 'info',
+	levels,
 	format: winston.format.combine(
 		winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-		winston.format.printf((info) => `${info.level}: ${[info.timestamp]}: ${info.message}`)
+		winston.format.printf((info) => `${[info.timestamp]}: ${info.level}: ${info.message}`)
 	),
 	transports: [
 		new winston.transports.File({
@@ -18,7 +36,7 @@ const logger = winston.createLogger({
 			maxsize: '10000000',
 			maxFiles: '10'
 		}),
-		new winston.transports.Console()
+		new winston.transports.Console({ format: winston.format.combine(winston.format.colorize({ all: true })) })
 	]
 });
 
