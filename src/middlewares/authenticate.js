c37dc94e4bc3a316edf5989144ae0929072744a6
de4th-zone <57558120+de4th-zone@users.noreply.github.com>
2021-09-16 20:56:16 +0700

    Update authenticate.js

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -3,9 +3,9 @@ import httpStatus from 'http-status';
 import APIError from '~/utils/apiError';
 import Role from '~/models/roleModel';
 
-const verifyCallback = (req, next, /* resolve, reject, */ requiredRights) => async (err, user, info) => {
+const verifyCallback = (req, resolve, reject, requiredRights) => async (err, user, info) => {
 	if (err || info || !user) {
-		return next(new APIError(httpStatus[httpStatus.UNAUTHORIZED], httpStatus.UNAUTHORIZED));
+		return reject(new APIError(httpStatus[httpStatus.UNAUTHORIZED], httpStatus.UNAUTHORIZED));
 	}
 	req.user = user;
 	if (requiredRights.length) {
@@ -21,24 +21,20 @@ const verifyCallback = (req, next, /* resolve, reject, */ requiredRights) => asy
 		//console.log('userRights: ', userRights);
 		//console.log('boolean: ', hasRequiredRights);
 		if (!hasRequiredRights) {
-			return next(new APIError('Resource access denied', httpStatus.FORBIDDEN));
+			return reject(new APIError('Resource access denied', httpStatus.FORBIDDEN));
 		}
 	}
-	return next();
+	return resolve();
 };
 
 const authenticate =
 	(...requiredRights) =>
 	async (req, res, next) => {
-		//		return new Promise((resolve, reject) => {
-		return passport.authenticate('jwt', { session: false }, verifyCallback(req, next, /* resolve, reject, */ requiredRights))(
-			req,
-			res,
-			next
-		);
+		return new Promise((resolve, reject) => {
+			passport.authenticate('jwt', { session: false }, verifyCallback(req, resolve, reject, requiredRights))(req, res, next);
+		})
+			.then(() => next())
+			.catch((err) => next(err));
 	};
-//		})
-//			.then(() => next())
-//			.catch((err) => next(err));
 
 export default authenticate;
